0919f5410e43ef91e13217cce83682b0e77cbbfe
Natali Ustinova <nata@purple.studio>
2025-02-20 20:15:46 +0300

    8kyu: century and initials

---
+++ Javascript/6kyu/initials.js
@@ -0,0 +1,10 @@
+function abbrevName(name){
+  const arr = name.toUpperCase().split(" ")
+  return arr[0].charAt(0) + "." + arr[1].charAt(0)
+}
+
+function abbrevName2(name){
+  return name.split(' ').map(i => i[0].toUpperCase()).join('.')
+}
+
+console.log(abbrevName("Peter Harris"))
